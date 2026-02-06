67cb1d5652ba17d1082dd928dd8207c612507101
mghebru <mghebru123@gmail.com>
2026-02-06 15:47:04 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { fetchProducts } from "./services/apiService";
 import { Product } from "./models/product";
 import { handleError } from "./utils/errorHandler";
 
+//fetch product data and convert into product objects
 async function main(): Promise<void> {
   try {
+    //fetching 
     const productsData = await fetchProducts();
-
+    
+    //convert data
     const products = productsData.map(
       (p) =>
         new Product(
@@ -18,11 +21,12 @@ async function main(): Promise<void> {
           p.stock
         )
     );
-
+    //display details for each product
     products.forEach((product) => product.displayDetails());
   } catch (error) {
+    //error handling
     handleError(error);
   }
 }
-
+//execute functiong
 main();
