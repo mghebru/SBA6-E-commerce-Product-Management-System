097d6fc05e89a35494fb1ad766d697f56269e35e
mghebru <mghebru123@gmail.com>
2026-02-06 15:38:06 -0500

    Update apiService.ts

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -4,15 +4,17 @@ const API_URL = "https://dummyjson.com/products";
 
 export async function fetchProducts(): Promise<any[]> {
   try {
+    //send request to products api
     const response = await fetch(API_URL);
-
+    //handle unsuccessful response
     if (!response.ok) {
       throw new AppError(
         `Failed to fetch products. Status: ${response.status}`
       );
     }
-
+    //parse response
     const data = await response.json();
+    //return products array
     return data.products;
   } catch (error) {
     throw error;
