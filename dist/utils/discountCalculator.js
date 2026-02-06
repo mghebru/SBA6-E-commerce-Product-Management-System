a611a5a357c52a49c2b2179b2ef399ca5c812c5f
mghebru <mghebru123@gmail.com>
2026-02-06 10:54:20 -0500

    Delete dist directory

--- dist/utils/discountCalculator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateDiscount = calculateDiscount;
-function calculateDiscount(price, discountPercentage) {
-    return (price * discountPercentage) / 100;
-}
-//# sourceMappingURL=discountCalculator.js.map
--- dist/utils/errorHandler.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class AppError extends Error {
-    constructor(message: string);
-}
-export declare function handleError(error: unknown): void;
-//# sourceMappingURL=errorHandler.d.ts.map
